256493259257c54e565ee8fbe5d064e864dbe523
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-07-27 12:33:40 +0800

    completed index.html

--- login_app/controllers/users.py
+++ login_app/controllers/users.py
@@ -1,3 +1,7 @@
 from login_app import app
 from flask import render_template,redirect,request,session,flash
 from login_app.models.user import User
+
+@app.route('/')
+def index():
+    return render_template('index.html')
