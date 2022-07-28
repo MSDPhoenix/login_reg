1a4c30f21aded890d06cbfedb156a9077bc6cc8c
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-07-28 13:09:12 +0800

    completed users.py, success.html

--- login_app/controllers/users.py
+++ login_app/controllers/users.py
@@ -5,3 +5,19 @@ from login_app.models.user import User
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/success/')
+def success():
+    
+    return render_template('success.html')
+
+@app.route('/register/',methods=['POST'])
+def register():
+
+    return redirect('/success/')
+
+@app.route('/login/',methods=['POST'])
+def login():
+
+    return redirect('/success/')
+
